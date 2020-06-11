996bb6441b1351711d71b7a92e7aa9b6c5953c2a
nitmohan <nithin.mohan@capgemini.com>
2020-06-11 15:59:33 +0530

    python

---
+++ greatest.py
@@ -0,0 +1,12 @@
+smallest = None
+
+for i in [10,9,7,4,12] :
+     if smallest is None:
+         smallest = i
+
+     elif smallest > i:
+         smallest = i
+
+      #   print(smallest)
+print(smallest)
+
